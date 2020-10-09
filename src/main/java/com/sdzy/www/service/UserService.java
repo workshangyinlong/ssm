749e1f03e37892aacbb574f83ee7def2883da966
yanghuiyi <https://gitee.com/yhy_520/my-git.git>
2020-10-09 16:24:34 +0800

    pojo,service

--- src/main/java/com/sdzy/www/service/UserService.java
+++ src/main/java/com/sdzy/www/service/UserService.java
@@ -1,7 +1,12 @@
 package com.sdzy.www.service;
 
+import com.sdzy.www.pojo.User;
+
 /*
     用户相关逻辑处理
  */
 public interface UserService {
+    User getTel(String tel);
+    void  insert(User user);
+
 }
