e070f4fc2ea89db2acb50f95f53699d227705025
Zhaoru <https://gitee.com/zhao-ru/xiao-zhaos-warehouse.git>
2020-10-09 17:06:11 +0800

    登录

--- src/main/java/com/sdzy/www/controller/UserController.java
+++ src/main/java/com/sdzy/www/controller/UserController.java
@@ -1,14 +1,11 @@
 package com.sdzy.www.controller;
 
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
 /*
 用户登录注册跳转以及结果处理处理器
 */
-
 
 @Controller
 @RequestMapping("/public")
@@ -17,4 +14,6 @@ public class UserController {
     public String loginPage(){
         return "login";
     }
+
+
 }
--- src/main/java/com/sdzy/www/mapper/UserMapper.java
+++ src/main/java/com/sdzy/www/mapper/UserMapper.java
@@ -5,4 +5,5 @@ package com.sdzy.www.mapper;
     用户dao层 格式为实体Mapper
 */
 public interface UserMapper {
+
 }
